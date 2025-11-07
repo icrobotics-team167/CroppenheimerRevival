77a786b89bb4174891cd4911f3c995622de6a096
RichWaWa <145046799+RichWaWa@users.noreply.github.com>
2025-11-06 18:47:55 -0600

    create branch

--- src/main/java/frc/cotc/superstructure/Pivot.java
+++ src/main/java/frc/cotc/superstructure/Pivot.java
@@ -1,6 +1,12 @@
+// Copyright (c) 2026 FRC 167
+// https://github.com/icrobotics-team167
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.cotc.superstructure;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Pivot extends SubsystemBase {
-}
+public class Pivot extends SubsystemBase {}
